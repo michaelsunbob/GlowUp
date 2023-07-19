0e59ea8940b2cc07bacc0157c92fd86867e7c123
michaelsunbob <michael.sun87@myhunter.cuny.edu>
2023-07-19 17:41:22 -0400

    updated css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,21 +2,21 @@ import { Link } from 'react-router-dom'
 
 export default function Navbar() {
     return (
-        <nav className='navbar'>
-            <ul className='navbar-list'>
-                <li className='item-title'>
-                    <Link to='/'>GlowUp</Link>
-                </li>
-                <li className='item'>
+        <nav className='nav'>
+            <li className='site-title'>
+                <Link to='/'>GlowUp</Link>
+            </li>
+            <ul>
+                <li>
                     <Link to='/routines'>Routines</Link>
                 </li>
-                <li className='item'>
+                <li>
                     <Link to='/products'>Products</Link>
                 </li>
-                <li className='item'>
+                <li>
                     <Link to='/recommendations'>Recommendations</Link>
                 </li>
-                <li className='item'>
+                <li>
                     <Link to='/account'>
                         <img src='profile-icon.png' alt='Profile Icon' className='profile-icon-img'></img>
                     </Link>
