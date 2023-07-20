0d27fcbf39baa0286da3c9c797c2f76eb41c706d
Keren Razo <kerenrazo@Kerens-MacBook-Air.local>
2023-07-19 21:54:58 -0400

    added a Quiz component and worked on homepage

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      
       <div className='container'>
         <Routes>
           <Route path='/' element={<Home />} />
