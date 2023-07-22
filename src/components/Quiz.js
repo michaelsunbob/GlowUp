fe44ae5b2da2e82a4db558f124156da4b2331911
Keren Razo <kerenrazo@Kerens-MacBook-Air.local>
2023-07-22 02:31:16 -0400

    worked more on quiz and structured folder for css and images we'll use

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,168 @@
-import React from "react";
+import React, { useState } from "react"
+import "../styles/quiz.css"
+import Home from "../pages/Home"
+
 
 const Quiz = () => {
+    const [age, setAge] = useState('')
+    const [skinType, setSkinType] = useState('')
+    const [skinConditions, setSkinConditions] = useState('')
+    const [skinConcerns, setSkinConcerns] = useState('')
+    const [currQuestion, setCurrQuestion] = useState(1) 
+
+    const handleNext = () => {
+        setCurrQuestion((onPrev) => onPrev + 1)
+    }
+    
+    const handlePrev = () => {
+        setCurrQuestion((onNext) => onNext - 1)
+    }
+
+    const handleStart = () => {
+       /* */ 
+    }
+    
+    const handleSubmit =() =>{
+        /* */
+    }
+
+
     return (
-        <div>
-             Question 1
-        </div>
+     <div className = "container">
+        {currQuestion === 1 && (
+            <div>
+                <h3 className = "question">Question 1</h3>
+                <h4>What is your age?</h4>
+                <form onSubmit ={handleSubmit}>
+                    <label>
+                    <input type="text" value={age} onChange={(e) => setAge(e.target.value)} />
+                    </label>
+                    <br/>
+                    <button onClick={handleStart}>Previous</button>
+                    <button onClick={handleNext}>Next</button>
+                </form>
+            </div>
+          )}
+
+        {currQuestion === 2 && (
+            <div>
+                <h3 className = "question">Question 2</h3>
+                <h4>What is your skin type?</h4>
+                <form onSubmit ={handleSubmit}>
+                    <label>
+                        <input type="radio" name='skinType' value='oily' checked={skinType === 'oily'} onChange={() => setSkinType('oily')} />
+                         Oily
+                    </label>
+                    <label>
+                        <input type="radio" name="skinType" value="dry" checked={skinType === 'dry'} onChange={() => setSkinType('dry')} />
+                        Dry
+                    </label>
+                    <label>
+                        <input type="radio" name="skinType" value="combination" checked={skinType === 'combination'} onChange={() => setSkinType('combination')} />
+                        Combination
+                    </label>
+                    <label>
+                        <input type="radio" name="skinType" value="normal" checked={skinType === 'normal'} onChange={() => setSkinType('normal')} />
+                        Normal
+                    </label>
+                    <br/>
+                    <button onClick={handlePrev}>Previous</button>
+                    <button onClick={handleNext}>Next</button>
+                </form>
+            </div>
+      )}  
+
+        {currQuestion === 3 && (
+            <div>
+                <h3 className = "question">Question 3</h3>
+                <h4>Do you have any skin conditions? Select all that apply</h4>
+                <form onSubmit ={handleSubmit}>
+                    <label>
+                        <input type="checkbox" value="None" checked={skinConditions.includes('None')} 
+                               onChange={() => setSkinConditions((prevConditions) =>prevConditions.includes('None') 
+                               ? prevConditions.filter((condition) => condition !== 'None') : [...prevConditions, 'None'])} />
+                                None
+                    </label>
+                    <label>
+                        <input type="checkbox" value="Psoriasis" checked={skinConditions.includes('Psoriasis')} 
+                               onChange={() =>setSkinConditions((prevConditions) => prevConditions.includes('Psoriasis') 
+                               ? prevConditions.filter((condition) => condition !== 'Psoriasis') : [...prevConditions, 'Psoriasis'])} />
+                               Psoriasis
+                    </label>
+                    <label>
+                        <input type="checkbox" value="Eczema" checked={skinConditions.includes('Eczema')} 
+                               onChange={() => setSkinConditions((prevConditions) => prevConditions.includes('Eczema') 
+                               ? prevConditions.filter((condition) => condition !== 'Eczema') : [...prevConditions, 'Eczema'])} />
+                               Eczema
+                    </label>
+                    <label>
+                        <input type="checkbox" value="Hyper-pigmentation" checked={skinConditions.includes('Hyper-pigmentation')}
+                               onChange={() => setSkinConditions((prevConditions) => prevConditions.includes('Hyper-pigmentation') 
+                               ? prevConditions.filter((condition) => condition !== 'Hyper-pigmentation') : [...prevConditions, 'Hyper-pigmentation'])} />
+                               Hyper-pigmentation
+                    </label>
+                    <label> 
+                        <input type="checkbox" value="Rosacea" checked={skinConditions.includes('Rosacea')}
+                               onChange={() => setSkinConditions((prevConditions) => prevConditions.includes('Rosacea') 
+                               ? prevConditions.filter((condition) => condition !== 'Rosacea') : [...prevConditions, 'Rosacea'])} />
+                               Rosacea
+                    </label>
+                    <br/>
+                    <button onClick={handlePrev}>Prev</button>
+                    <button onClick={handleNext}>Next</button>
+                </form>
+            </div>
+        )}
+
+        {currQuestion === 4 && (
+            <div>
+                <h3 className = "question">Question 4</h3>
+                <h4>Do you have any skin concerns? Select all that apply. </h4>
+                <form onSubmit ={handleSubmit}>
+                    <label>
+                        <input type="checkbox" value="None" checked={skinConcerns.includes('None')} 
+                               onChange={() => setSkinConcerns((prevConcerns) => prevConcerns.includes('None') 
+                               ? prevConcerns.filter((concern) => concern !== 'None') : [...prevConcerns, 'None'])} />
+                                None
+                    </label>
+                    <label>
+                        <input type="checkbox" value="Acne and Blemishes" checked={skinConcerns.includes('Acne and Blemishes')}
+                               onChange={() => setSkinConcerns((prevConcerns) => prevConcerns.includes('Acne and Blemishes') 
+                               ? prevConcerns.filter((concern) => concern !== 'Acne and Blemishes') : [...prevConcerns, 'Acne and Blemishes'])} />
+                               Acne and Blemishes 
+                    </label>
+                    <label>
+                        <input type="checkbox" value="Anti-Aging" checked={skinConcerns.includes('Anti-Aging')} 
+                                onChange={() => setSkinConcerns((prevConcerns) => prevConcerns.includes('Anti-Aging') 
+                                ? prevConcerns.filter((concern) => concern !== 'Anti-Aging') : [...prevConcerns, 'Anti-Aging'])} />
+                                Anti-Aging
+                    </label>
+                    <label>
+                        <input type="checkbox" value="Blackheads" checked={skinConcerns.includes('Blackheads')}
+                               onChange={() => setSkinConcerns((prevConcerns) => prevConcerns.includes('Blackheads') 
+                               ? prevConcerns.filter((concern) => concern!== 'Blackheads') : [...prevConcerns, 'Blackheads'])} />
+                                Blackheads
+                    </label>
+                    <label>
+                        <input type="checkbox" value="Dark Circles" checked={skinConcerns.includes('Dark Circles')}
+                               onChange={() => setSkinConcerns((prevConcerns) => prevConcerns.includes('Dark Circles') 
+                               ? prevConcerns.filter((concern) => concern !== 'Dark Circles') : [...prevConcerns, 'Dark Circles'])} />
+                               Dark Circles
+                    </label>
+                    <label>
+                        <input type="checkbox" value="Dullness" checked={skinConcerns.includes('Dullness')}
+                               onChange={() => setSkinConcerns((prevConcerns) => prevConcerns.includes('Dullness') 
+                               ? prevConcerns.filter((concern) => concern !== 'Dullness') : [...prevConcerns, 'Dullness'])} />
+                               Dullness
+                    </label>
+                    <br/>
+                    <button onClick={handlePrev}>Prev</button>
+                    <br/>
+                    <button type = "submit" >Submit </button>
+                </form>
+            </div>
+        )}
+    </div> 
     )
 };
 
