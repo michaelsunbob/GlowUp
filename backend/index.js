68aae4900d500c19a59658653463d413249898b8
Keren Razo <kerenrazo@Kerens-MacBook-Air.local>
2023-07-28 02:51:31 -0400

    added backend

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express();
+app.use(cors());
+
+
+
+
+
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
