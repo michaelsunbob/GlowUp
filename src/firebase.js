7720c508b5b627011a9e9083e13eacaa9147c100
Emma Haque <emmaha299@gmail.com>
2023-07-31 19:49:26 -0400

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCZzStYNtHWmNmXMyOKYebl4cPv2TN8kJo",
+  authDomain: "react-550c0.firebaseapp.com",
+  projectId: "react-550c0",
+  storageBucket: "react-550c0.appspot.com",
+  messagingSenderId: "646908889125",
+  appId: "1:646908889125:web:a1af917b9a1883436917cc",
+  measurementId: "G-LPJ8WQFLE0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
