d24f5606f9c82db6b70fc485e85109c3e0a9f6d2
Emma Haque <emmaha299@gmail.com>
2023-08-07 23:29:09 -0400

    quiz data onto account page

--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -1,5 +1,43 @@
+import { getFirestore, collection, getDocs} from "firebase/firestore";
+import React, { useState, useEffect } from "react"
+
+
+
 export const LoggedIn = () => {
+    const [quizR, setQuizR] = useState([]);
+    const db = getFirestore()
+    const colRef = collection(db, "quizResults")
+    getDocs(colRef).then((snapshot) =>{
+        let results = []
+        snapshot.docs.forEach((result) => {
+            results.push({...result.data(), id: result.id})
+        })
+    }).catch(err =>{
+        console.log(err.message)
+    })
+
+    useEffect(()=> {
+        const quizRe = async () => {
+            const data = await getDocs(colRef)
+            setQuizR(data.docs.map((doc, i) => ({...doc.data(), id: doc.id})))
+        };
+        quizRe();
+    }, [colRef])
+
     return(
-        <p>{`Signed in`}</p>
-    )
+        <div>
+            {quizR.map((quiz) => {
+                return (
+                    <div>
+                        <h1 style={{ textDecoration: 'none', color:'black' }}>Id: {quiz.userId} </h1> 
+                        <h1 style={{ textDecoration: 'none', color:'black' }}>Recommended: {quiz.recommendedProducts}</h1>
+                    </div>
+                );    
+            })}
+
+            <p>{`Signed in`}</p>
+        </div>
+        
+
+    );
 }
