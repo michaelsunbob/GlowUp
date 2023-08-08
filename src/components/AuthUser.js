3ebf8a96812d0db9b77b0a27927986b542ab305e
Emma Haque <emmaha299@gmail.com>
2023-08-08 18:06:07 -0400

    login page personalization update

--- src/components/AuthUser.js
+++ src/components/AuthUser.js
@@ -3,6 +3,7 @@ import React, {useState ,useEffect} from "react";
 import { auth } from "../firebase";
 import { Login } from "./Login"
 import { LoggedIn } from "./LoggedIn";
+import { Link } from "react-router-dom"
 
 const AuthUser = () => {
     const [AuthUser, setAuthUser] = useState(null);
@@ -30,6 +31,17 @@ const AuthUser = () => {
             {
             AuthUser ? 
             <><LoggedIn/>
+            <br>
+            </br>
+            <h1 style={{color: '#A679CA' }}>WELCOME!</h1>
+            <p style = {{color: '#B19CD8'}}>{`You are signed in as ${AuthUser.email}`}</p>
+            <br>
+            </br>
+            <p style = {{color: '#B19CD8'}}>
+                Take the quiz! <Link to="/quiz">Quiz Link</Link>
+            </p>
+            <br>
+            </br>
             <button 
                 onClick = {authSignOut}>Sign Out
             </button>
--- src/components/LoggedIn.js
+++ src/components/LoggedIn.js
@@ -1,5 +1,8 @@
 import { getFirestore, collection, getDocs} from "firebase/firestore";
 import React, { useState, useEffect } from "react"
+import  { ref, uploadBytes, getDownloadURL } from "firebase/storage"
+import { storage } from "../firebase"
+import { Avatar } from '@mui/material';
 
 
 
@@ -7,6 +10,33 @@ export const LoggedIn = () => {
     const [quizR, setQuizR] = useState([]);
     const db = getFirestore()
     const colRef = collection(db, "quizResults")
+
+    const [image, setImage] = useState(null)
+    const [url, setURL] = useState(null)
+
+    const handleImageChange = (e) =>{
+        if(e.target.files[0]){
+            setImage(e.target.files[0])
+        }
+
+    };
+
+    const handleSubmit = () => {
+        const img = ref(storage, "image")
+        uploadBytes(img, image).then(() => {
+            getDownloadURL(img).then((url)=> {
+                setURL(url);
+            }).catch(error => {
+                console.log(error.message, "error getting image")
+            });
+            setImage(null);
+        }).catch((error => {
+            console.log(error.message)
+        }));
+    };
+
+    
+
     getDocs(colRef).then((snapshot) =>{
         let results = []
         snapshot.docs.forEach((result) => {
@@ -20,24 +50,23 @@ export const LoggedIn = () => {
         const quizRe = async () => {
             const data = await getDocs(colRef)
             setQuizR(data.docs.map((doc, i) => ({...doc.data(), id: doc.id})))
-        };
+        }; 
         quizRe();
     }, [colRef])
-
     return(
         <div>
-            {quizR.map((quiz) => {
-                return (
-                    <div>
-                        <h1 style={{ textDecoration: 'none', color:'black' }}>Id: {quiz.userId} </h1> 
-                        <h1 style={{ textDecoration: 'none', color:'black' }}>Recommended: {quiz.recommendedProducts}</h1>
-                    </div>
-                );    
-            })}
-
-            <p>{`Signed in`}</p>
+            <Avatar style = {{left: 80}}
+                alt = "Remy Sharp"
+                src = {url}
+                sx = {{width: 150, height: 150}}
+            />
+            <br>
+            </br>
+            <input style = {{left:80}}type = "file" onChange = {handleImageChange}/> 
+            <br>
+            </br>
+            <button onClick = {handleSubmit}>Submit</button>
         </div>
-        
-
     );
+
 }
