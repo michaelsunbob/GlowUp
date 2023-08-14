1fa209538f2478639a852abf5df7b60ab4fd9d6c
Emma Haque <emmaha299@gmail.com>
2023-08-13 21:24:23 -0400

    login css

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,8 @@ import {signInWithEmailAndPassword} from "firebase/auth"
 import "../styles/navbar.css"
 import "../styles/login.css"
 import { auth } from "../firebase";
+import { BiSolidUser, BiSolidLockAlt } from "react-icons/bi";
+
 
 export const Login = () => {
     const [username, setUsername] = useState('')
@@ -44,21 +46,36 @@ export const Login = () => {
                     </p>
                 </section>
             ) : (
-                <section>
-                    <p ref={errRef} className={errorMessage ? "errmsg" : "offscreen"} aria-live="assertive">{errorMessage}</p>
-                    <h1>Login</h1>
-                    <form onSubmit={handleSubmit}>
-                        <label htmlFor="text">E-mail: </label>
-                        <input type="email" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="glowingUser@chi.com" name="username" required ref ={usernameRef}></input>
-                        <label htmlFor="password">Password: </label>
-                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} name="password" required></input>
-                        <button type = "submit" >Login</button>
-                    </form>
-                    <br></br>
-                    <li>
-                            <Link style={{ textDecoration: 'none', color:'black' }} to='/register'>Register</Link>
-                    </li>
-                </section>
+                    <body>
+                        <section>
+                            <p ref={errRef} className={errorMessage ? "errmsg" : "offscreen"} aria-live="assertive">{errorMessage}</p>
+                            <div class = "wrapper">
+                                <form onSubmit={handleSubmit}>
+                                    <h1 style={{color:"#6B2E32"}}>Login</h1>
+                                    <div className = "input-box">
+                                        <label htmlFor="text">E-mail: </label>
+                                        <input type="email" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="glowingUser@chi.com" name="username" required ref ={usernameRef}></input>
+                                        <BiSolidUser className = "i"/>
+                                    </div>
+                                    <div className = "input-box">
+                                        <label htmlFor="password">Password: </label>
+                                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder = "Password" name="password" required></input>
+                                        <BiSolidLockAlt className = "i"/>
+                                    </div>
+                                    <div className= "remember-forgot">
+                                        <label><input type = "checkbox"/>Remember me</label>
+                                    </div>
+                                    <br></br>
+                                    <button className = "button" type = "submit" >Login</button>
+                                </form>
+                                <br></br>
+                                <div class = "register-link">
+                                    <p style={{color:'#a1797c'}}>Don't have an account? <Link style={{color:'#a1797c' }} to='/register'>Register here</Link></p>
+                                </div>
+                            </div>
+                        </section>
+                    </body>
+                
             )}
 
         </>
